0a0fbf56f0a210034703cf5433efde1f4d1f4ba2
byteErudite <raghuvanshivibhu@gmail.com>
2025-05-27 14:46:33 +0530

    Added basic grpc service endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,43 @@
-fn main() {
-    let user = String::from("vaibhav");
-    let mut test_vector = Vec::new();
-    test_vector.push("string1");
+use proto::calculator_server::{Calculator, CalculatorServer};
+use tonic::transport::Server;
 
-    test_vector.iter().for_each(|value| println!("{}", value));
-    println!("Hello, {}", user);
+pub mod proto {
+    tonic::include_proto!("calculator");
+}
+
+#[derive(Debug, Default)]
+struct CalculatorService {}
+
+#[tonic::async_trait]
+impl Calculator for CalculatorService {
+    async fn add(
+        &self, 
+        request: tonic::Request<proto::CalculationRequest>,
+    ) -> Result<tonic::Response<proto::CalculaionResponse>, tonic::Status> {
+
+        print!("Got a request: {:?}", request);
+        let input = request.get_ref();
+
+        let response = proto::CalculaionResponse {
+            result : input.a + input.b,
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+}
+
+#[tokio:: main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
+    let addr = "127.0.0.1:50051".parse()?;
+
+    let calc = CalculatorService::default();
+
+    Server::builder()
+    .add_service(CalculatorServer::new(calc))
+    .serve(addr)
+    .await?;
+
+    Ok(())
+    
 }
